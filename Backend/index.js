4bc69b2d44e3f5532b09d3f0d7a14c7309ee8b88
sima <simalike245@gmail.com>
2024-04-03 05:42:58 -0700

    corrected second

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+mongoose.connect('mongodb+srv://sima:1W238DFMvSh50S8M@cluster0.rz199gl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+});
+   
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+app.get('/users', (req, res) => {
+    res.json({a:"hvhv"}) 
+  });
+  
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
