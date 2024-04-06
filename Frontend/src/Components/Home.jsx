a5737ffaba6d5dd4f44c5acdb3bcf340ac1c954c
sima <simalike245@gmail.com>
2024-04-06 04:52:56 -0700

    see notification button

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { useGettUsersQuery} from '../store/Features/api/apiSlice';
 import Alert from '@mui/material/Alert';
 import CheckIcon from '@mui/icons-material/Check';
+import { Notifications } from '@mui/icons-material';
+import { Badge } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 const Home = () => {
   const { data, error, isLoading } = useGettUsersQuery();
 
@@ -19,16 +22,13 @@ const Home = () => {
         <img src="/logo.png" alt="Company Logo" className="h-8 w-8 mr-2" />
         <span className="text-white">Company Name</span>
       </div>
-      <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
-  Here is a gentle confirmation that your action was successful.
-</Alert>
+        <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+            Here is a gentle confirmation that your action was successful.
+        </Alert>
       <div className="flex items-center">
-        <div className="mr-4">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-          </svg>
-          <span className="bg-red-500 text-white rounded-full h-4 w-4 flex justify-center items-center ml-2">5</span>
-        </div>
+        <Badge badgeContent={4} color="error">
+            <Notifications />
+        </Badge>
         <div>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
@@ -40,9 +40,7 @@ const Home = () => {
           </svg>
         </div>
         <div className="ml-4">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5z" />
-          </svg>
+            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
         </div>
       </div>
     </div>
